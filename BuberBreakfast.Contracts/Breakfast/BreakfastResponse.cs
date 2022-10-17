78059e6245ebdddb129363ac3d3c607a5d5a573f
Bui Huu Dat <dat54261001@gmail.com>
2022-10-18 04:49:24 +0700

    first push dotnet project

---
+++ BuberBreakfast.Contracts/Breakfast/BreakfastResponse.cs
@@ -0,0 +1,12 @@
+namespace BuberBreakfast.Contracts.Breakfast;
+
+public record BreakfastResponse(
+  Guid Id,
+  string Name,
+  string Description,
+  DateTime StartDateTime,
+  DateTime EndDateTime,
+  DateTime LastModifiedDateTime,
+  List<string> Savory,
+  List<string> Sweet
+);
---
+++ BuberBreakfast/Program.cs
@@ -0,0 +1,13 @@
+using BuberBreakfast.Services.Breakfasts;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+  builder.Services.AddControllers();
+  builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+}
+var app = builder.Build();
+{
+  app.UseHttpsRedirection();
+  app.MapControllers();
+  app.Run();
+}
